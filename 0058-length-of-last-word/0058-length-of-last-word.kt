4ae3386966c257b8018edd1b2a13475237d34386
Chris <jaehun6004@gmail.com>
2025-05-28 00:02:22 +0900

    Time: 0 ms (100%), Space: 41.4 MB (74.84%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.kt
+++ 0058-length-of-last-word/0058-length-of-last-word.kt
@@ -1,10 +1,17 @@
 class Solution {
     fun lengthOfLastWord(s: String): Int {
-        val words = s.trim().split(" ")
-        return if (words.isNotEmpty()) {
-            words.last().length
-        } else {
-            0
+        var len = 0
+        var i = s.length - 1
+
+        while(i >= 0 && s[i] == ' ') {
+            i--
         }
+
+        while(i >= 0 && s[i] != ' ') {
+            len++
+            i--
+        }
+
+        return len;
     }
 }
