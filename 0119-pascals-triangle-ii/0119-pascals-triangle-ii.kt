7523e06909b44e686ed5a8b4669dcddc63fb5ece
Chris <jaehun6004@gmail.com>
2025-06-25 00:02:44 +0900

    Time: 7 ms (46.71%), Space: 41.6 MB (50.66%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.kt
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.kt
@@ -1,22 +1,16 @@
 class Solution {
+    // chatGPT 참고 후 개선
     fun getRow(rowIndex: Int): List<Int> {
-        if (rowIndex == 0) return listOf(1)
+        var result = mutableListOf(1)
 
-        val result = mutableListOf<List<Int>>()
-
-        for (i in 0..rowIndex) {
-            val row = MutableList(i + 1) { 1 } // 모든 요소를 1로 초기화
-            if (i >= 2) {
-                val prev = result[i - 1]
+        for (i in 1..rowIndex) {
+            val row = MutableList(i + 1) { 1 }
                 for (j in 1 until i) {
-                    row[j] = prev[j - 1] + prev[j]
+                    row[j] = result[j - 1] + result[j] // 첫 반복문은 1 until 1이라 실행되지 않음 -> [1, 1]
                 }
-            }
-            result.add(row)
+            result = row
         }
 
-        println(result)
-
-        return result[rowIndex]
+        return result
     }
 }
