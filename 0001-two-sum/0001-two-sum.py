afb957408f2f4728819e609274e6adb4d0fbf94c
Chris <jaehun6004@gmail.com>
2025-09-23 23:20:52 +0900

    Time: 0 ms (100%), Space: 18.9 MB (33.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,7 +3,9 @@ class Solution:
         seen = {} # 지나온 [값, 인덱스] 기록
 
         for (i, num) in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
+            complement = target - num # 필요한 값
+            if complement in seen: # 필요한 값(key)이 지금 seen에 있나?
+                return [seen[complement], i] # 있다면 [해당 값의 index, 현재 index] 로 return
+            seen[num] = i # 없다면 seen[해당 값] = 현재 index
+
+        # 문제 구조 상 for문이 다 돌때까지 못찾을 일은 없으니 따로 마지막 return은 없어도 됨.
